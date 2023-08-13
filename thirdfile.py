2724850f80a29cc914c9dcade88bbe4c7f24bd71
Maslah Ali <71393974+maslah25@users.noreply.github.com>
2023-08-13 18:44:54 -0500

    added some code

--- thirdfile.py
+++ thirdfile.py
@@ -1,1 +1,10 @@
-ECHO is on.
+customer_profile={"name":"NA","age":"NA","address":"NA","email":"NA","phone":"NA"}
+
+userinput={"name":"Maslah","age":15,"address":"Ohio","email":"mas@gmail.com",
+           "phone":"666-668-2555"}
+
+#ask_user= input("Please enter, your name,age,address,email,phone:\n")
+
+customer_profile|=userinput
+
+print(customer_profile)
