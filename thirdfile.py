dec6c942b2fc4034748bf9fd205219a36d2d31b6
Maslah Ali <71393974+maslah25@users.noreply.github.com>
2023-08-13 19:12:19 -0500

    added some code

--- thirdfile.py
+++ thirdfile.py
@@ -1,10 +1,26 @@
-customer_profile={"name":"NA","age":"NA","address":"NA","email":"NA","phone":"NA"}
+def storing_customers_info():
+    customer_profile={"name":"NA","age":"NA",
+                      "address":"NA","email":"NA","phone":"NA"}
 
-userinput={"name":"Maslah","age":15,"address":"Ohio","email":"mas@gmail.com",
-           "phone":"666-668-2555"}
+    userinput={"name":"Maslah","age":15,
+               "address":"Ohio","email":"mas@gmail.com",
+            "phone":"666-668-2555"}
 
-#ask_user= input("Please enter, your name,age,address,email,phone:\n")
+    customer_profile|=userinput
+    print(customer_profile)
 
-customer_profile|=userinput
+    askname=input("Enter your name:\n")
+    ask_age= int(input("Enter your age:\n"))
+    ask_address= input("Enter your address:\n")
+    ask_email= input("Enter your email:\n")
+    ask_phone= input("Enter your phone number:\n")
 
-print(customer_profile)
+    customer_profile["name"],customer_profile["age"],customer_profile["address"],customer_profile["email"]
+    customer_profile["phone"]=askname,ask_age,ask_address,ask_email,ask_phone
+
+    saveresult= customer_profile
+    print(saveresult)
+    return saveresult
+
+if __name__=="__main__":
+    storing_customers_info()
