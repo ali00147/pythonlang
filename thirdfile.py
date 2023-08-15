4f2b68ae6a37b748a328448891abe1c86292bacd
Maslah Ali <71393974+maslah25@users.noreply.github.com>
2023-08-14 21:15:56 -0500

    modified thirdfile.py

--- thirdfile.py
+++ thirdfile.py
@@ -1,26 +1,38 @@
-def storing_customers_info():
-    customer_profile={"name":"NA","age":"NA",
-                      "address":"NA","email":"NA","phone":"NA"}
 
-    userinput={"name":"Maslah","age":15,
+profile={"name":"NA","age":"NA",
+                      "address":"NA","email":"NA","phone":"NA"}
+userinput={"name":"Maslah","age":15,
                "address":"Ohio","email":"mas@gmail.com",
             "phone":"666-668-2555"}
 
-    customer_profile|=userinput
+profile|=userinput
+print(profile)
+    
+def storing_customers_info():
+    users_input=[]
+    customer_profile={}
+    
+    result_of_data_entered={}
+    while True:
+        
+        customer_profile["Name"]=input("Enter your Name:\n")
+        customer_profile["Age"]=input("Enter your Age:\n")
+        customer_profile["Address"]=input("Enter your Current Address:\n")
+        customer_profile["City"]=input("Enter the City:\n")
+        customer_profile["State"]=input("Enter The State:\n")
+        customer_profile["Email"]=input("Enter your Email Address:\n")
+        customer_profile["Phone"]=input("Enter your Phone number:\n")
+
+        users_input.append(customer_profile)
+
+        user_response= input("Would like to contiune entering data")
+        if user_response =="no":
+            print("Thank you, see you soon!",customer_profile)
+            break
+        else:
+            continue
     print(customer_profile)
-
-    askname=input("Enter your name:\n")
-    ask_age= int(input("Enter your age:\n"))
-    ask_address= input("Enter your address:\n")
-    ask_email= input("Enter your email:\n")
-    ask_phone= input("Enter your phone number:\n")
-
-    customer_profile["name"],customer_profile["age"],customer_profile["address"],customer_profile["email"]
-    customer_profile["phone"]=askname,ask_age,ask_address,ask_email,ask_phone
-
-    saveresult= customer_profile
-    print(saveresult)
-    return saveresult
+    return customer_profile
 
 if __name__=="__main__":
     storing_customers_info()
