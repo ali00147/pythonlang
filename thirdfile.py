7c7cbec807fca63cc989b20b0648cc4f4a9973e5
Maslah Ali <71393974+maslah25@users.noreply.github.com>
2023-08-15 22:29:11 -0500

    added somechanges

--- thirdfile.py
+++ thirdfile.py
@@ -8,31 +8,70 @@ userinput={"name":"Maslah","age":15,
 profile|=userinput
 print(profile)
     
-def storing_customers_info():
-    users_input=[]
-    customer_profile={}
+def create_user_account():
+    uniqueChars = "$%&*@!.,|:"
+    numbers = "123456789"
+    chars = "abcdefghijklmnopqrstuvwxyz"
     
-    result_of_data_entered={}
     while True:
+        user_name = input("Please choose a username (must have six characters, including at least one number and one letter): ")
         
-        customer_profile["Name"]=input("Enter your Name:\n")
-        customer_profile["Age"]=input("Enter your Age:\n")
-        customer_profile["Address"]=input("Enter your Current Address:\n")
-        customer_profile["City"]=input("Enter the City:\n")
-        customer_profile["State"]=input("Enter The State:\n")
-        customer_profile["Email"]=input("Enter your Email Address:\n")
-        customer_profile["Phone"]=input("Enter your Phone number:\n")
-
-        users_input.append(customer_profile)
-
-        user_response= input("Would like to contiune entering data")
-        if user_response =="no":
-            print("Thank you, see you soon!",customer_profile)
-            break
-        else:
+        if len(user_name) != 6:
+            print("Username must have exactly six characters.")
             continue
-    print(customer_profile)
-    return customer_profile
+        
+        has_number = any(char.isdigit() for char in user_name)
+        has_char = any(char.isalpha() for char in user_name)
+        
+        if not (has_number and has_char):
+            print("Username must contain at least one number and one letter.")
+            continue
+        
+        break
 
-if __name__=="__main__":
-    storing_customers_info()
+    while True:
+        password = input("Please enter a password (must have at least one unique character: $%&*@!.,|:): ")
+
+        if not any(char in uniqueChars for char in password):
+            print("Password must contain at least one unique character: $%&*@!.,|:")
+            continue
+
+        break
+
+    saveusername = [user_name]
+    savepassword = [password]
+    print("Success, you have created your account.")
+
+    print(f' your user name is {saveusername}and your password is {savepassword}')
+    return saveusername, savepassword
+def storing_customers_info():
+    customers = []
+
+    while True:
+        customer_profile = {}
+        customer_profile["Name"] = input("Enter your Name:\n")
+        customer_profile["Age"] = int(input("Enter your Age:\n"))
+        customer_profile["Address"] = input("Enter your Current Address:\n")
+        customer_profile["City"] = input("Enter the City:\n")
+        customer_profile["State"] = input("Enter The State:\n")
+        customer_profile["Email"] = input("Enter your Email Address:\n")
+        customer_profile["Phone"] = input("Enter your Phone number:\n")
+
+        customers.append(customer_profile)
+
+        user_response = input("Would you like to continue entering data? (yes/no): ")
+        if user_response.lower() == "no":
+            print("Thank you, see you soon!")
+            break
+
+    for customer in customers:
+        print("Customer Profile:")
+        for key, value in customer.items():
+            print(f"{key}: {value}")
+        print("-" * 20)
+
+    return customers
+
+if __name__ == "__main__":
+    #storing_customers_info()
+    create_user_account()
