c8ae2f24ce0bf05e29c62e9eab162ef7e8153db2
Maslah Ali <71393974+maslah25@users.noreply.github.com>
2023-08-18 22:31:53 -0500

    modfied some code

--- fourt_file.py
+++ fourt_file.py
@@ -1,5 +1,6 @@
- fruits={"apple","apple", "apple","orange","orange","tomato","tomato","pen","pen","mas"}
+fruits=["apple","apple", "apple","orange","orange",
+        "tomato","tomato","pen","pen","mas"]
 
- count_fruits={fruit: fruits.count(fruit) for fruit in fruits}
- fruits:list[str]
- print(count_fruits)
+count_fruits = {fruit: fruits.count(fruit) for fruit in fruits}
+fruits:list[str]
+print(count_fruits)
