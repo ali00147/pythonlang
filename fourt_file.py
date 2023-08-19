d5bddbbc9fe5f4ebd812d1725709880fa217185f
Maslah Ali <71393974+maslah25@users.noreply.github.com>
2023-08-18 21:53:22 -0500

    added a new file named fourth_file

---
+++ fourt_file.py
@@ -0,0 +1,5 @@
+ fruits={"apple","apple", "apple","orange","orange","tomato","tomato","pen","pen","mas"}
+
+ count_fruits={fruit: fruits.count(fruit) for fruit in fruits}
+ fruits:list[str]
+ print(count_fruits)
