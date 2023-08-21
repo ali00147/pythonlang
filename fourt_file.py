51f769eb8a16a00cd10cbbca728a3d01059941b5
Maslah Ali <71393974+maslah25@users.noreply.github.com>
2023-08-20 21:18:54 -0500

    added some more code

--- fourt_file.py
+++ fourt_file.py
@@ -1,6 +1,23 @@
+from collections import Counter
+from collections import defaultdict
+
 fruits=["apple","apple", "apple","orange","orange",
         "tomato","tomato","pen","pen","mas"]
 
 count_fruits = {fruit: fruits.count(fruit) for fruit in fruits}
 fruits:list[str]
 print(count_fruits)
+
+#//--------using the counter libary-----------//
+print("-"*70)
+count_fruits2 = Counter(fruits)
+print(count_fruits2)
+
+#//---------using collections defaultdict------//
+
+count_fruits = defaultdict(int)
+for fruit in fruits:
+    count_fruits[fruit] += 1
+
+print("-"*70)
+print("defaultdict:",dict(count_fruits))
